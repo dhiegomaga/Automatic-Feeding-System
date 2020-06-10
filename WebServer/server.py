f99505c7791cedd5aa9d49cf5f987e2e18447c65
Dhiego <dhiegomaga@gmail.com>
2020-06-10 21:47:33 +0200

    Add AJAX requests to server

--- WebServer/server.py
+++ WebServer/server.py
@@ -1,14 +1,14 @@
 import sys
 sys.path.insert(0,'../SerialCommunicationProtocol')
 from SerialProtocolSimple import SerialProtocol
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,jsonify
 
 app = Flask(__name__)
 
-systen_variables={
+system_variables={
+    'temp0': 0,
     'temp1': 0,
     'temp2': 0,
-    'temp3': 0,
 
     'cooler_set': 0,
     'heater_set': 0,
@@ -19,39 +19,43 @@ systen_variables={
 
 @app.route('/')
 def index():
-    global systen_variables
+    global system_variables
 
     # Read GPIO Status
-    systen_variables['temp1'] = '55 C'
+    system_variables['temp1'] = '55 C'
 
     system_state = 'ON'
-    if systen_variables['system_on'] == False:
+    if system_variables['system_on'] == False:
         system_state = 'OFF'
 
     templateData = {
-        'temp1'  : systen_variables['temp1'],
+        'temp0'  : system_variables['temp0'],
+        'temp1'  : system_variables['temp1'],
+        'temp2'  : system_variables['temp2'],
+
+        'pump_set': system_variables['pump_set'],
         'system_state': system_state
     }
     return render_template('index.html', **templateData)
 
-@app.route("/<deviceName>/<action>")
-def action(deviceName, action):
-    global systen_variables
+@app.route("/debug")
+def debug_vlues():
+    global system_variables
+    system_variables['temp0'] = '85 C'
+    return jsonify({
+        'result':'ok'
+    })
 
-    if deviceName == 'button1':
-        systen_variables['system_on'] = not systen_variables['system_on']
-   
-    system_state = 'ON'
-    systen_variables['temp1'] = '54 C'
-    if systen_variables['system_on'] == False:
-        system_state = 'OFF'
-        systen_variables['temp1'] = '0 C'
+@app.route("/setPumpSpeed", methods = ['POST'])
+def setPumpSpeed():
+    global system_variables
 
-    templateData = {
-        'temp1'  : systen_variables['temp1'],
-        'system_state': system_state
-    }
-    return render_template('index.html', **templateData)
+    system_variables['pump_set'] = request.json['value']
+
+    return jsonify({
+        'result':'ok'
+    })
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
